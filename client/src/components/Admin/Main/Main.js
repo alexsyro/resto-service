5b6324e70cfbf83e7c25e18eee7e9daaab55d9b2
Musa <guseinovmus@gmail.com>
2021-09-17 12:03:28 +0300

    ComponentOrders

--- client/src/components/Admin/Main/Main.js
+++ client/src/components/Admin/Main/Main.js
@@ -1,17 +1,11 @@
 import { Route, Switch } from 'react-router';
 import styles from './Main.module.scss';
+import Orders from './Orders/Orders';
 
 function Main() {
   return (
     <div className={styles.container}>
-      <Switch>
-        <Route to='/'>
-          <p>Главная страница админки</p>
-        </Route>
-        <Route to='/???'>
-          <p>Управление чем-то :)</p>
-        </Route>
-      </Switch>
+      <Orders />
     </div>
   );
 }
---
+++ client/src/components/Admin/Main/Orders/Orders.js
@@ -0,0 +1,11 @@
+import styles from './Orders.module.scss';
+
+function Orders() {
+  return (
+    <div className={styles.container}>
+      Компонент заказов
+    </div>
+  );
+}
+
+export default Orders;
