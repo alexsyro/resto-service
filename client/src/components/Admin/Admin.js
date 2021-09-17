0040e08b03e1b03a9f48defcaf66f2b5c6665333
Musa <guseinovmus@gmail.com>
2021-09-17 12:31:52 +0300

    промежуточный коммит перед слиянием админа

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -5,30 +5,32 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Menu from './Menu/Menu';
+import Orders from './Main/Orders/Orders'
+import Staff from './Staff/Staff'
 
 function Admin() {
   return (
     <div className={styles.container}>
+      <Router>
+        <Header />
+        <SideMenu />
 
-      <Header />
-      <SideMenu />
-
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/menu" exact>
-          <Menu />
-        </Route>
-        <Route path="/orders" exact>
-          <Orders />
-        </Route>
-        <Route path="/staff" exact>
-          <Staff />
-        </Route>
-      </Switch>
-      <Footer />
-
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/menu" exact>
+            <Menu />
+          </Route>
+          <Route path="/orders" exact>
+            <Orders />
+          </Route>
+          <Route path="/staff" exact>
+            <Staff />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router >
 
     </div>
   );
