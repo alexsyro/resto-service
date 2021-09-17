aeb0e2747de52b1500dfe0e174e7c230da9d9d9d
Sergey <trezxqee@gmail.com>
2021-09-17 17:40:30 +0300

    fix: fetch for login

--- client/src/components/Client/LoginForm/LoginForm.js
+++ client/src/components/Client/LoginForm/LoginForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { sagaAC } from '../../../redux/actionCreators/loginFormAC';
+// import { sagaAC } from '../../../redux/actionCreators/loginFormAC';
 import styles from './LoginForm.module.scss';
+import { GET_USER } from '../../../redux/actionTypes/actionType'
 
 function LoginForm() {
 
@@ -9,15 +10,26 @@ function LoginForm() {
 
   const loginSubmit = (e) => {
     e.preventDefault();
-    dispatch(sagaAC());
+    // dispatch(sagaAC());
+    const { action, method, email, password } = e.target;
+    fetch(action, {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+      body: {
+        email: email.value,
+        password: password.value
+      }
+    })
+    .then(res => res.json())
+    .then(data => dispatch({type: GET_USER, action: data}))
   };
 
   return (
-    <form className={styles.center} action="/login" method="POST" name="loginForm" onSubmit={loginSubmit}>
-      <input type="text" name="email" placeholder="Email"/>
+    <form className={styles.center} action="http://localhost:1234/login" method="POST" name="loginForm" onSubmit={loginSubmit}>
+      <input type="text" name="email" placeholder="Email" />
       {/* <input type="text" name="login" placeholder="Enter your login"/> */}
       {/* <input type="phone" name="phone" placeholder="Enter your phone"/> */}
-      <input type="password" name="password" placeholder="Enter your password"/>
+      <input type="password" name="password" placeholder="Enter your password" />
       <button type="submit">Войти</button>
     </form>
   )
