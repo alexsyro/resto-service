9ddb90e29e68ab3d745cbfad7396c5765218abec
Sergey <trezxqee@gmail.com>
2021-09-22 21:43:16 +0300

    feat:add main/nav/menu scss

--- client/src/components/Admin/Menu/Category/Category.js
+++ client/src/components/Admin/Menu/Category/Category.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Subcategory from '../Subcategory/Subcategory'
+import styles from './Category.module.scss';
 
 function Category({category}) {
 
   return (
     <>
       <p>{category.name}</p>
+      <div className={styles.subcategory}>
       {category.categories.map((category)=> <Subcategory key={category.id} subcategory={category}/>)}
+      </div>
     </>
   );
 }
