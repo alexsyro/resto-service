fc61ba8247735f21293e04c8b3df8e0ea8c11165
Igor <igor.dudin.2013@mail.ru>
2021-09-22 19:11:32 +0300

    fix: menu styles and html architecture

--- client/src/components/Client/Menu/Menu.js
+++ client/src/components/Client/Menu/Menu.js
@@ -4,9 +4,7 @@ import styles from './Menu.module.scss'
 import menu from '../../../images/menu.png'
 
 function Menu() {
-
   const [actualCategory, setActualCategory] = useState('MENU') // через useState меняем категорию меню и в зависимости от этого рендерим компонент
-
 
   return (
     <>
@@ -15,27 +13,18 @@ function Menu() {
       {/* условный рендеринг ниже */}
 
       <div className={styles.container}>
-        <div >
-          <img className={styles.pic}
-            src={menu}
-            alt="menu" />
+        <div>
+          <img className={styles.pic} src={menu} alt="menu" />
         </div>
 
         <div className={styles.block}>
-
-        <div className={styles.categories}>
-          {actualCategory === 'MENU' && <CategoryList />}
+          <div className={styles.categories}>
+            {actualCategory === 'MENU' && <CategoryList />}
+          </div>
         </div>
-
-        <div className={styles.menu_picture}>
-        </div>
-
-        </div>
-
       </div>
-
     </>
-  );
+  )
 }
 
 export default Menu
