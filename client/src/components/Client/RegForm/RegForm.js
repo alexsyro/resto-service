96f9b463583026a947a4b069df995452d06e5e9e
Ksenia <ks.nikolaychuk@gmail.com>
2021-09-23 19:36:19 +0300

    feat: translation is done

--- client/src/components/Client/RegForm/RegForm.js
+++ client/src/components/Client/RegForm/RegForm.js
@@ -3,10 +3,13 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { sagaRegAC } from '../../../redux/actionCreators/sagaAC';
 import styles from './RegForm.module.scss';
+import { useTranslation } from 'react-i18next';
+
 
 const checkEmptyFields = (fields) => fields.every((field) => field.value.length);
 
 function RegForm() {
+  const { t, i18n } = useTranslation();
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -39,24 +42,24 @@ function RegForm() {
           <div className={styles.group}>
             <input type='text' name='name' required />
             <span className={styles.bar}></span>
-            <label>Ваше имя</label>
+            <label>{t('reg.1')}</label>
           </div>
           <div className={styles.group}>
             <input type='text' name='email' required />
             <span className={styles.bar}></span>
-            <label>Ваш email</label>
+            <label>{t('reg.2')}</label>
           </div>
           <div className={styles.group}>
             <input type='phone' name='phone' required />
             <span className={styles.bar}></span>
-            <label>Ваш номер телефона</label>
+            <label>{t('reg.3')}</label>
           </div>
           <div className={styles.group}>
             <input type='password' name='password' required />
             <span className={styles.bar}></span>
-            <label>Пароль</label>
+            <label>{t('reg.4')}</label>
           </div>
-          <button type='submit'>Зарегистрироваться</button>
+          <button type='submit'>{t('reg.5')}</button>
         </form>
       </div>
     </div>
